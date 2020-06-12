f4cff838d5e388bafcf3bc62edacf43ecdaad5ec
Jarkko Haikarainen <jarkko.haikarainen@student.careeria.fi>
2020-06-12 22:52:10 +0300

    hyvä ope

--- Opas.py
+++ Opas.py
@@ -3,5 +3,6 @@ print("Tämä on opas Gitin versionhallintaan")
 opas=input("Auttaako tämä opas sinua Gitin hallinnassa?: ")
 if opas=="yes":
     print("Hyvä, niin ajattelinkin")
+    print("Sinä hyvä oppilas, minä hyvä opettaja..")
 else:
     print("Tutki sitten YouTubesta lisää!")
