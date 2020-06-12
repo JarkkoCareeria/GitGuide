ca500ebcdd86382ac87478995525f4863b09b48b
Jarkko Haikarainen <jarkko.haikarainen@student.careeria.fi>
2020-06-12 15:14:20 +0300

    Koodattiin Opas.py tiedostoon kysely

--- Opas.py
+++ Opas.py
@@ -1,1 +1,7 @@
 print("Tämä on opas Gitin versionhallintaan")
+
+opas=input("Auttaako tämä opas sinua Gitin hallinnassa?: ")
+if opas=="yes":
+    print("Hyvä, niin ajattelinkin")
+else:
+    print("Tutki sitten YouTubesta lisää!")
